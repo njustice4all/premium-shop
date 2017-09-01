110c1f2c46bd3dd9aa72c85bf35e46dd3f8923d3
youngjoo <hyj@aty.kr>
2017-09-01 11:34:12 +0900

    Wrapping with redux

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import App from './containers/App';
 
@@ -7,9 +9,11 @@ export default class Root extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     );
   }
 };
