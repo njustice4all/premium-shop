46ac2186e574a76179aad612126b78ad861b255e
youngjoo <hyj@aty.kr>
2017-09-28 10:44:16 +0900

    Published franchiseList page

--- src/containers/main/FranchiseList.js
+++ src/containers/main/FranchiseList.js
@@ -2,14 +2,72 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Redirect } from 'react-router-dom';
 
+const cnt = [1, 2, 3, 4, 5, 6, 7, 8];
+const List = () => {
+  return (
+    <div className="lists">
+      <div className="content-wrapper">
+        <div className="icon">
+          <i className="fa fa-pencil-square-o" aria-hidden="true" />
+        </div>
+        <div className="image-wrapper">
+          <i className="fa fa-camera" aria-hidden="true" />
+        </div>
+        <div className="describe-wrapper">
+          <div className="franchise-name">
+            <h1>가맹점명</h1>
+          </div>
+          <div className="franchise-tag">
+            <p>아메리카노, 더치커피, 카페모카</p>
+          </div>
+          <div className="franchise-address">
+            <p>서울특별시 서초구 강남대로 3333-000</p>
+          </div>
+          <div className="franchise-contact">
+            <p>02-000-3232</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 class FranchiseList extends Component {
+  onBackPress = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     const { authentication } = this.props;
-    if (!authentication.isLogin) {
-      return <Redirect to="/auth/signin" />;
-    }
+    // if (!authentication.isLogin) {
+    //   return <Redirect to="/auth/signin" />;
+    // }
 
-    return <h1>리스트</h1>;
+    return (
+      <div className="franchise-list">
+        <header>
+          <span onClick={this.onBackPress}>
+            <i className="fa fa-angle-left" aria-hidden="true" />
+          </span>
+          <h1>가맹점 목록 (000)</h1>
+        </header>
+        <div className="franchise-list__container">
+          <div className="franchise-list__search">
+            <span className="icon-wrapper">
+              <i className="fa fa-search" aria-hidden="true" />
+            </span>
+            <input className="search-input-wrapper" type="text" />
+          </div>
+          <div className="divider">
+            <div />
+          </div>
+          <div className="franchise-list__list-wrapper">
+            {cnt.map((franchise, index) => <List key={`franchise-${index}`} />)}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/containers/main/Main.js
+++ src/containers/main/Main.js
@@ -14,28 +14,28 @@ class Main extends Component {
     // }
 
     return (
-      <div style={styles.container}>
-        <div style={styles.columnWrapper} onClick={this.onAddButtonPress}>
+      <div className="main__container">
+        <div className="column-wrapper" onClick={this.onAddButtonPress}>
           <div>
             <div style={{ display: 'flex' }}>
-              <div style={styles.iconPlus}>+</div>
-              <h1 style={styles.title}>가맹점등록</h1>
+              <div className="icon-plus">+</div>
+              <h1 className="title">가맹점등록</h1>
             </div>
             <div style={{ marginTop: '15px' }}>
-              <p style={styles.description}>새로운 가맹점을 등록합니다.</p>
+              <p className="description">새로운 가맹점을 등록합니다.</p>
             </div>
           </div>
         </div>
-        <div style={{ ...styles.columnWrapper, ...styles.bottom }} onClick={this.onListButtonPress}>
+        <div className="column-wrapper bottom" onClick={this.onListButtonPress}>
           <div>
             <div>
-              <h1 style={styles.title}>가맹점목록</h1>
+              <h1 className="title">가맹점목록</h1>
             </div>
             <div>
-              <h2 style={styles.franchiseCount}>876</h2>
+              <h2 className="franchise-count">876</h2>
             </div>
             <div>
-              <p style={styles.description}>등록된 가맹점 정보를 수정합니다.</p>
+              <p className="description">등록된 가맹점 정보를 수정합니다.</p>
             </div>
           </div>
         </div>
@@ -51,60 +51,3 @@ const mapStateToProps = state => {
 };
 
 export default withRouter(connect(mapStateToProps)(Main));
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    flex: '1',
-    justifyContent: 'center',
-    textAlign: 'center',
-    height: '100%',
-    backgroundImage: 'url(/img/img01.png)',
-    backgroundSize: 'cover',
-  },
-  columnWrapper: {
-    backgroundColor: '#fc7f1a',
-    height: '125px',
-    marginBottom: '15px',
-    width: '72%',
-    display: 'flex',
-    alignSelf: 'center',
-    borderRadius: '10px',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'white',
-  },
-  bottom: {
-    backgroundColor: 'white',
-    margin: 0,
-    color: 'black',
-  },
-  iconPlus: {
-    width: '30px',
-    height: '30px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
-    fontSize: '30px',
-    fontWeight: '100',
-    backgroundColor: 'white',
-    color: '#fc7f1a',
-    borderRadius: '15px',
-  },
-  title: {
-    marginLeft: '10px',
-    fontSize: '24px',
-    fontWeight: '400',
-    lineHeight: '34px',
-  },
-  description: {
-    fontSize: '14px',
-    // fontWeight: '300',
-  },
-  franchiseCount: {
-    fontSize: '30px',
-    color: '#fc7f1a',
-    margin: '5px 0',
-  },
-};
